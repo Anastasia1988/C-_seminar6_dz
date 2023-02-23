72c20e6b40b40ccf171d62331c608c46718a3cb4
Dovidovich Anastasia <sidorova88-88@mail.ru>
2023-02-23 15:42:45 +0300

    dz

---
+++ zadacha41/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+//  сколько чисел больше 0 ввёл пользователь.
+//  0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+int [] ReadInt()
+{
+    System.Console.WriteLine("Введите M чисел: ");
+    int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    return array;
+}
+int numPozitiv(int[] array)
+{
+    
+    int numberPozitiv = new int();
+    int numberNegativ = new int();
+    for(int i=0; i<array.Length; i++)
+    {
+
+        if(array[i]>0)
+        {
+            numberPozitiv ++;
+        }
+        else
+        {
+            numberNegativ ++;
+        }
+    }
+    return numberPozitiv;
+}
+
+int[] myArray = ReadInt();
+System.Console.WriteLine($"Введено положитетельных чисел :{numPozitiv(myArray)} ");
+
+
+
+
---
+++ zadacha43/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и
+//  k2 задаются пользователем.
+//  b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double Readint(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+var k1 = Readint("Введите значение k1 прямой direct1");
+var b1 = Readint("Введите значение b1 прямой direct1");
+var k2 = Readint("Введите значение k2 прямой direct2");
+var b2 = Readint("Введите значение b2 прямой direct2");
+
+
+
+double x = 0;
+double y = 0;
+
+
+x = Math.Round((-(b1 - b2) / (k1 - k2)), 3);
+y = Math.Round((k1 * x + b1), 3);
+System.Console.WriteLine($"Пересечение двух прямых будет в точке: {x};{y}");
+
+
+
+
+
